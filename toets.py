95ab67f401c0884b93484cbb08d4ef8db9aa5679
RoyOtten <99065498@mydavinci.nl>
2021-09-27 14:02:34 +0200

    eerste commit

---
+++ toets.py
@@ -0,0 +1,9 @@
+print ("welkom bij de sollicitatie") 
+print ("we beginnen gelijk met de vragen")
+vraagacteursdiploma = input ("heb je een acteursdiploma") 
+vraagacteursdiplomaoke = vraagacteursdiploma + ("yes") 
+vraag18 = input ("hoe oud ben je?") 
+vraag18oke = (vraag18 > 18) 
+if vraag18oke: 
+    print ("mooi") 
+    
